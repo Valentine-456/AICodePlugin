3fb68546db39b22f04be07e2164c7b536d429991
Valentyn Bondarenko <murica.it@gmail.com>
2026-05-08 16:58:37 +0200

    Clean up boilerplate code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.tasks.RunIdeTask
+import java.util.Properties
+
+val envFile = rootProject.file(".env")
+val envProps = Properties().apply {
+    if (envFile.exists()) envFile.inputStream().use { load(it) }
+}
+fun env(key: String): String = envProps.getProperty(key) ?: System.getenv(key) ?: ""
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
@@ -6,8 +14,13 @@ plugins {
     id("org.jetbrains.changelog")
 }
 
+tasks.withType<RunIdeTask> {
+    envProps.forEach { k, v -> environment(k.toString(), v.toString()) }
+}
+
 dependencies {
     testImplementation("junit:junit:4.13.2")
+    implementation("ai.koog:koog-agents:0.7.1")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
